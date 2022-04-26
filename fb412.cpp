212254ef48e18173167eaba44105a94decfc95fb
crashALot <ofni.yratilim@gmail.com>
2022-04-25 21:46:28 -0400

    Include line added for merge conflict

--- fb412.cpp
+++ fb412.cpp
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 58850b03232bb94e32712814f4c512d17638986b
 /*******************************************************************************
  * File: fb412.cpp
  * Author: Glenn Kiefer
@@ -7,13 +10,16 @@
  *              file for included methods.
  *
  *      Given an integer n, return a string array answer (1-indexed) where:        
- *	answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
- *	answer[i] == "Fizz" if i is divisible by 3.
- *	answer[i] == "Buzz" if i is divisible by 5.
- *	answer[i] == i (as a string) if none of the above conditions are true. 		
+ *	    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ *	    answer[i] == "Fizz" if i is divisible by 3.
+ *	    answer[i] == "Buzz" if i is divisible by 5.
+ *	    answer[i] == i (as a string) if none of the above conditions are true. 		
 *******************************************************************************/
+<<<<<<< HEAD
 
 
 =======
 #include
 >>>>>>> 5125a4d655055ca1b5989232dad9ecc87b4667e4
+=======
+>>>>>>> 58850b03232bb94e32712814f4c512d17638986b
