afad42a307d54f96e0cdd0105e61fd76e777dfdb
crashALot <ofni.yratilim@gmail.com>
2022-04-26 19:39:24 -0400

    Actually added main program file fb412main

--- fb412main.cpp
+++ fb412main.cpp
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * File: fb412main.cpp
+ * Author: Glenn Kiefer
+ * Date: 26 April 2022
+ * Description: Fizzbuzz problem 412 - Main program file. See associated fb412.cpp
+ *              file for included functions.
+ *
+ *      Given an integer n, return a string array answer (1-indexed) where:        
+ *	    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ *	    answer[i] == "Fizz" if i is divisible by 3.
+ *	    answer[i] == "Buzz" if i is divisible by 5.
+ *	    answer[i] == i (as a string) if none of the above conditions are true. 		
+*******************************************************************************/
 #include fb412.hpp
 
 int main ()
