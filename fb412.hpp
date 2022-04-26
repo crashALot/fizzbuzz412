5945ebc1b3b0520ff2c8bda039262a3e9e695ff6
crashALot <ofni.yratilim@gmail.com>
2022-04-26 18:26:23 -0400

    Added class: Solution per problem set

--- fb412.hpp
+++ fb412.hpp
@@ -11,4 +11,9 @@
  *	answer[i] == i (as a string) if none of the above conditions are true. 		
 *******************************************************************************/
 
-
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+    }
+};
